318bb8f5c83ad37bb76bb1bd00602f82d5dd74f8
David Tischler <david@balena.io@users.noreply.github.com>
2021-06-09 14:25:16 -0700

    Added new demo and model

--- openvino/start.sh
+++ openvino/start.sh
@@ -4,4 +4,4 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 X &
 xfce4-session &
 source /opt/intel/openvino_2021.3.394/bin/setupvars.sh >> ~/.bashrc
-python3 /opt/intel/openvino_2021.3.394/deployment_tools/open_model_zoo/demos/object_detection_demo/python/object_detection_demo.py -i /dev/video0 -m /opt/intel/openvino_2021.3.394/deployment_tools/open_model_zoo/demos/models/intel/$MODEL/FP32/$MODEL.xml -at ssd -r -d CPU --labels labels.txt
+python3 /opt/intel/openvino_2021.3.394/deployment_tools/open_model_zoo/demos/$DEMO/python/$DEMO.py -i /dev/video0 -m /opt/intel/openvino_2021.3.394/deployment_tools/open_model_zoo/demos/models/intel/$MODEL/FP32/$MODEL.xml -at $ARCHITECTURE_TYPE -r -d CPU
