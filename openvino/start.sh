c5b1bc06cc4066a29cce55c1e1430267b653735c
dtischler <{ID}+{username}@users.noreply.github.com>
2021-07-02 13:54:18 -0700

    update to 2021.4.582

--- openvino/start.sh
+++ openvino/start.sh
@@ -3,5 +3,6 @@ export DISPLAY=:0
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 X &
 xfce4-session &
-source /opt/intel/openvino_2021.3.394/bin/setupvars.sh >> ~/.bashrc
-python3 /opt/intel/openvino_2021.3.394/deployment_tools/open_model_zoo/demos/$DEMO/python/$DEMO.py -i /dev/video0 -m /opt/intel/openvino_2021.3.394/deployment_tools/open_model_zoo/demos/models/intel/$MODEL/FP32/$MODEL.xml -at $ARCHITECTURE_TYPE -r -d CPU
+source /opt/intel/openvino_2021.4.582/bin/setupvars.sh >> ~/.bashrc
+python3 /opt/intel/openvino_2021.4.582/deployment_tools/open_model_zoo/demos/$DEMO/python/$DEMO.py -i /dev/video0 -m /opt/intel/openvino_2021.4.582/deployment_tools/open_model_zoo/demos/models/intel/$MODEL/FP16/$MODEL.xml -at $ARCHITECTURE_TYPE -d CPU
+
